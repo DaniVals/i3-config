6100c645d1a397b498210d1b5e645aefaf8236c4
shovelflow_L <shovelflow@gmail.com>
2025-11-27 14:02:21 +0100

    ADD: bases mensaje de ayuda y barra superior

---
+++ help-messages.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+
+case $1 in
+	apps)
+		i3-nagbar -m 'APPS:'
+		;;
+	ventanas)
+		i3-nagbar -m 'VENTANAS: CTR+'
+		;;
+	movimientos)
+		i3-nagbar -m 'MOVIMIENTOS: '
+		;;
+	*)
+		# TAMBIEN ES LA DE INICIO
+		i3-nagbar -m 'HOLAAAAAAAA :33333' \
+			-B 'movimientos' "$0 movimientos; exit" \
+			-B 'ventanas' "$0 ventanas; exit" \
+			-B 'apps' "$0 apps; exit" \
+		;;
+esac
